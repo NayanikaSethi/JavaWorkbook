9b388910f7cf6d2d1f5e443ea370200b67498ab4
NayanikaSethi <nayanikasethi06@gmail.com>
2025-11-16 17:33:25 +0530

    Add files via upload

---
+++ EXP7/TestPackageDemo.java
@@ -0,0 +1,10 @@
+import exercises.java.PackageDemo;
+
+public class TestPackageDemo {
+    public static void main(String[] args) {
+        PackageDemo pd = new PackageDemo();
+        pd.display();
+
+        System.out.print("Nayanika Sethi 24csu137");
+    }
+}
